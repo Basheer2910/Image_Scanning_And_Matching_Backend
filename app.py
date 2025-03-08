e1d2b690d5bbfa40ad75037a71e9c20264cc4bb2
Basheer2910 <syed.basheer@pw.live>
2025-03-08 21:00:36 +0530

    Accept PDF files and detect text in it

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ from werkzeug.utils import secure_filename
 from flask_cors import CORS
 import shutil
 import asyncio
+from pdf2image import convert_from_path
+POPPLER_PATH = "/opt/homebrew/bin"
 app = Flask(__name__)
 CORS(app)
 
@@ -21,7 +23,6 @@ def runOCRPrintedModel(script, args):
     
     if process.returncode == 0:
         text = stdout.strip()
-        print(text, " printed text")
         return text[6:] if text[:6] == 'text: ' else ""
     else:
         print(f"Error in {script}: {stderr}")
@@ -36,15 +37,13 @@ def runHandwrittenModel(script, args):
     
     if process.returncode == 0:
         text = stdout.strip()
-        return text
+        return  text
     else:
         print(f"Error in {script}: {stderr}")
         return None
 
 def runSimilarityCheckerModel(script, args):
-    print(args, " argssss")
     script_path = os.path.join(MODELS_FOLDER, script)
-    print(['python', script_path] + args, " runninggggg")
     process = subprocess.Popen(['python', script_path] + args, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
     stdout, stderr = process.communicate()
     
@@ -54,9 +53,30 @@ def runSimilarityCheckerModel(script, args):
     else:
         print(f"Error in {script}: {stderr}")
         return None
+    
+async def runPDFModel(script, args):
+    script_path = os.path.join(MODELS_FOLDER, script)
+    pdf_path = os.path.abspath(args[0])
+
+
+    pdf_output_folder = os.path.join(os.getcwd(), 'PDF')
+    os.makedirs(pdf_output_folder, exist_ok=True)
+
+    images = convert_from_path(pdf_path, dpi=600, poppler_path=POPPLER_PATH)
+    image_paths = []
+    pages = [""] * len(images)
+    
+    for i, image in enumerate(images):
+        image_filename = f"{os.path.splitext(os.path.basename(pdf_path))[0]}_page_{i}.png"
+        image_path = os.path.join(pdf_output_folder, image_filename)
+        image.save(image_path, "PNG")
+        image_paths.append(image_path)
+        pages[i]=runHandwrittenModel(script_path, [image_path])
+    return ' '.join(pages)
+    
 
 @app.route('/process-images', methods=['POST'])
-def process_images():
+async def process_images():
     os.makedirs(UPLOAD_FOLDER, exist_ok=True)
     if 'student_image' not in request.files or 'answer_key' not in request.files:
         return jsonify({'error': 'Please upload both student and answer key images.'}), 400
@@ -66,29 +86,31 @@ def process_images():
 
     student_image_filename = secure_filename(student_image.filename)
     answer_key_filename = secure_filename(answer_key.filename)
-    print(student_image_filename, answer_key_filename, "   images folder name")
 
     student_image_path = os.path.join(UPLOAD_FOLDER, student_image_filename)
     answer_key_path = os.path.join(UPLOAD_FOLDER, answer_key_filename)
-    print(student_image_path, answer_key_path)
     student_image.save(student_image_path)
     answer_key.save(answer_key_path)
     
     print("Processing uploaded student and answer key images...")
-    
-    student_answer = runHandwrittenModel('handwritten_model.py', [student_image_path])
+    student_answer=""
+    if student_image_path.lower().endswith('.pdf'):
+        student_answer = await runPDFModel('handwritten_model.py', [student_image_path, 'PDF'])
+    else:
+        student_answer = runHandwrittenModel('handwritten_model.py', [student_image_path])
     key = runOCRPrintedModel('ocr_printed_model.py', [answer_key_path])
-    print(student_answer, " student Answer")
-    print(key, " keyyy")
+    print(student_answer, "         - student Answer")
+    print(key, "         - Answer Key")
     if student_answer is None or key is None:
         return jsonify({'error': 'Error processing images.'}), 500
     
     score = runSimilarityCheckerModel('similarity_checker_model.py', [student_answer, key])
-    
+    print(score, "         - Score")
     if score is None:
         return jsonify({'error': 'Error comparing answers.'}), 500
     
     shutil.rmtree(UPLOAD_FOLDER, ignore_errors=True)
+    shutil.rmtree(os.path.join(os.getcwd(), 'PDF'), ignore_errors=True)
     shutil.rmtree(SEGMENTS_FOLDER, ignore_errors=True)
     
     return jsonify({'data': {'score': score}})
